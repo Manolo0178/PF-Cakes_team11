dd6a73ede0b55d3241cca7cf5beeece357da778f
greko3450 <johannsm03@gmail.com>
2023-05-09 15:36:58 -0500

    index modificado

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
+let productRouter = require("./productRouter.js")
+let dessertRouter = require("./dessertRouter.js")
+ 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use("/products", productRouter);
+router.use("/desserts", dessertRouter);
 
 
 module.exports = router;
