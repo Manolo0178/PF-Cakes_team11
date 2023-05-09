3818cbc1d41ad8e50e81e49ea45fdb513a68a7de
Tomas Sorgetti <tomassorgetti456@gmail.com>
2023-05-09 11:17:25 -0300

    prueba git

---
+++ client/src/Views/LandingPage/landingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "react-router-dom";
+// import styles from "./landingPage.module.css"
+
+
+function Landing() {
+    return (
+        <div>
+            <img src="" alt="" />
+            <Link to="/home">Ingresar</Link>
+        </div>
+    )
+}
+export default Landing
