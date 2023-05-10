fc65d6eb24a5e5002c2ed714686e8f4bcac5383d
horacio cano <hhoraciocano@gmail.com>
2023-05-10 19:34:13 -0300

    commit de front

---
+++ client/src/Views/Products/Products.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Cards from "../../components/Cards/cards";
+import Navbar from "../../components/Navbar/Navbar";
+
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { getAllProducts } from "../../redux/actions/index";
+
+function Products() {
+    const dispatch = useDispatch();
+    const products = useSelector((state) => state.allProducts);
+ // falta products
+    useEffect(() => {
+      dispatch(getAllProducts());
+    }, []);
+    
+  return (
+      <div>
+        <Navbar/>
+          <h1>Productos</h1>
+          <div>
+              <div>
+                  <h5>Categorias</h5>
+              </div>  
+            <Cards products={products} />
+          </div>
+      </div>
+  );
+}
+export default Products;
---
+++ client/src/components/Card/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Card({product}) {
+    // falta card
+    const { name, image, precio } = product;
+    return (
+        <div>
+            <img src={image} alt="product" /> 
+            <h3>{name}</h3>
+            <p>{precio}</p> 
+            <button>agregar al carrito</button>
+        </div>
+    )
+}
+export default Card
+
