523ac78d9b18a50f9029c72072fb7173716b237b
Josema996 <101608821+Josema996@users.noreply.github.com>
2023-05-09 23:22:30 -0300

    NavBar y carrusel modificar tamaño

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import Carousel from "react-bootstrap/Carousel";
+import Carousel from 'react-bootstrap/Carousel';
 import { useState } from "react";
 
 import carousel1 from "../../assets/images/carrousel1.jpg"
@@ -6,6 +6,7 @@ import carousel2 from "../../assets/images/carrousel2.jpg"
 import carousel3 from "../../assets/images/carrousel3.jpg"
 
 import styles from "./Carousel.module.css"
+
 function ControlledCarousel() {
   const [index, setIndex] = useState(0);
 
@@ -16,14 +17,14 @@ function ControlledCarousel() {
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <img className="d-block w-100" src={carousel1} alt="First slide" />
+        <img className="d-block w-100" src={carousel1} alt="First slide" style={{height: "650px", width: "auto"}}/>
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={carousel2} alt="Second slide" />
+        <img className="d-block w-100" src={carousel2} alt="Second slide" style={{height: "650px", width: "auto"}} />
 
         <Carousel.Caption>
           <h3>Second slide label</h3>
@@ -31,7 +32,7 @@ function ControlledCarousel() {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={carousel3} alt="Third slide" />
+        <img className="d-block w-100" src={carousel3} alt="Third slide" style={{height: "650px", width: "auto"}} />
 
         <Carousel.Caption>
           <h3>Third slide label</h3>
