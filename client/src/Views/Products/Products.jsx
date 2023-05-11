a9edb53a36fe60dfb3ed454ae96021fdd7a63d66
TomasSorgetti <97346262+TomasSorgetti@users.noreply.github.com>
2023-05-11 15:42:47 -0300

    cambios css

--- client/src/Views/Products/Products.jsx
+++ client/src/Views/Products/Products.jsx
@@ -8,6 +8,8 @@ import Cards from "../../components/Cards/cards";
 
 import { getAllProducts } from "../../redux/actions";
 
+import styles from "./Products.module.css"
+
 function Products() {
     const dispatch = useDispatch()
     const products = useSelector(state => state.allProducts)
@@ -16,21 +18,31 @@ function Products() {
         dispatch(getAllProducts());
     }, [dispatch])
     
-    console.log(products)
 
     return (
+      <div className={styles.products}>
+        <NavBar />
         <div>
-            <NavBar />
-            <div>
-                <h1>Products</h1>
-                <section>
-                    <div>
-                        <h5>Categorías</h5>
-                    </div>
-                    <Cards products={products} />
-                </section>
+          <h1>Products</h1>
+          <section className={styles.cont}>
+            <div className={styles.category}>
+              <h5>Categorías</h5>
             </div>
+            <div className={styles.cardsCont}>
+                <div className={styles.orderBy}>
+                    <label htmlFor="">ordenar por:</label>
+                    <select>
+                        <option value="">Precio: Menor a Mayor</option>
+                        <option value="">Precio: Mayor a Menor</option>
+                        <option value="">A-Z</option>
+                        <option value="">Z-A</option>
+                    </select>                    
+                </div>
+                <Cards products={products} />
+            </div>
+          </section>
         </div>
-    )
+      </div>
+    );
 }
 export default Products
