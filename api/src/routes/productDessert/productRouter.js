d0b8f105ea7e6288e474bc7ac9940538203331a4
greko3450 <johannsm03@gmail.com>
2023-05-10 19:53:38 -0500

    se actualizo

--- api/src/routes/productDessert/productRouter.js
+++ api/src/routes/productDessert/productRouter.js
@@ -6,94 +6,103 @@ const {Product, Dessert} = require("../../db.js")
 
 productRouter.get("/:idProduct", async (req, res) => {
 const { idProduct } = req.params
+try {
+  const getProductById = await Product.findOne({
+    where: {
+      id: idProduct
 
-  try {
-    const getProductById = await Product.findOne({
-      where: {
-        id: idProduct
-
-      },
-      include: [
-        {
-          model: Dessert,
-          attributes: ["name"],
-          through: { attributes: [] },
-        }
-      ]
-    });
-
-    if (getProductById) {
-
-      return res.status(200).send(getProductById)
-
-    } else {
-      return res.status(404).json({ error: { message: "Product doesn't exist", value: { ...req.params } } });
-    }
-  } catch (error) {
-    console.log(error.message);
-
-
+    },
+    include: [
+      {
+        model: Dessert,
+        attributes: ["name"],
+        through: { attributes: [] },
+      }
+    ]
+  });
+  if (getProductById) {
+    return res.status(200).send(getProductById)
+  } else {
+    return res.status(404).json({ error: { message: "Product doesn't exist", value: { ...req.params } } });
   }
+} catch (error) {
+  console.log(error.message);
+}
 })
 
 
 productRouter.get("/", async (req, res) => {
-  try {  
-    const { name } = req.query;
-    if(name){
-      const productBDd = await Product.findAll({
-        where:{
-          name:{
-            [Op.iLike]: `%${name}%`,
-          }
-        },
-        include: Dessert,
-      })
-      if(!productBDd){
-        res.status(404).json({message: `Product name not found ${name}`})
-      }else{
-        res.status(200).json(productBDd)
-      }
+try {  
+  const { name } = req.query;
+  if(name){
+    const productBDd = await Product.findAll({
+      where:{
+        name:{
+          [Op.iLike]: `%${name}%`,
+        }
+      },
+      include: Dessert,
+    })
+    if(!productBDd){
+      res.status(404).json({message: `Product name not found ${name}`})
     }else{
-      const productBdd = await Product.findAll()
-      res.status(200).json(productBdd);
+      res.status(200).json(productBDd)
     }
-
-  } catch (error) {
-    res.status(500).json({message: error.message})
+  }else{
+    const productBdd = await Product.findAll({
+      include: {
+        model: Dessert,
+        attributes: ["name"],
+        through: { attributes: [] },
+      }
+    })
+    res.status(200).json(productBdd);
   }
+} catch (error) {
+  res.status(500).json({message: error.message})
+}
 })
-
-
 
 productRouter.post("/", async(req, res) => {
-  try {
-    let {name, summary, description, image, price, desserts} = req.body;
-    
-      const newProduct = await Product.create({
-        name,
-        description,
-        summary,
-        image,
-        price
-      })
-      if(Array.isArray(desserts)) {
-        const dessertInstances = await Promise.all(desserts.map( async dessert => {
-          const [dessertInstances] = await Dessert.findOrCreate({where: {name: dessert}})
-          return dessertInstances
-          
-        }))
-        await newProduct.addDesserts(dessertInstances)
-        res.status(200).json(newProduct)
-      } else {
+try {
+  let {name, summary, description, image, price, desserts} = req.body;
+  
+    const newProduct = await Product.create({
+      name,
+      description,
+      summary,
+      image,
+      price
+    })
+    if(Array.isArray(desserts)) {
+      const dessertInstances = await Promise.all(desserts.map( async dessert => {
+        const [dessertInstances] = await Dessert.findOrCreate({
+          where: {name: dessert}})
+        return dessertInstances
+        
+      }))
+      await newProduct.addDesserts(dessertInstances)
+      res.status(200).json(newProduct)
+    } else {
 
-        res.status(404).json({message: "product not Created"})
-      }
-
-      
+      res.status(404).json({message: "product not Created"})
+    }
   } catch (error) {
     res.status(500).json({message: error.message})
   }
-
 })
+
 module.exports = productRouter;
+      
+
+          
+
+      
+
+
+
+
+
+
+
+
