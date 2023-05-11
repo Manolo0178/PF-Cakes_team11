55342bed1c696e9d650ff3f377377091cdab57ee
greko3450 <johannsm03@gmail.com>
2023-05-10 19:36:04 -0500

    se agregó la rutta por desserts

--- api/src/routes/productDessert/dessertRouter.js
+++ api/src/routes/productDessert/dessertRouter.js
@@ -2,4 +2,17 @@ const express = require('express');
 const dessertRouter = express.Router()
 const {Dessert} =  require("../../db.js")
 
+dessertRouter.get("/", async(req, res) => {
+  try {
+    let dessertsAll = await Dessert.findAll()
+    if(dessertsAll) {
+      res.status(200).json(dessertsAll.map(dessert => dessert.name))
+    } else {
+      res.status(404).json({message: "no desserts"})
+    }
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
 module.exports = dessertRouter
