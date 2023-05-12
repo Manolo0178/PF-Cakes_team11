9708129c5967b3434634a0b2e2fb1df7b4d15a4f
AylenRondan <rrondancarrizo@gmail.com>
2023-05-12 17:43:28 -0300

    Creo ruta a create

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,8 @@ function App() {
         <Route path="/Products" component={Products} />
         <Route path="/createUser" component={CreateUser} />
         <Route path="/login" component={Login} />
-        <Route exact path="/home/about" component={About} />
+        <Route path="/about" component={About} />
+        <Route path="/create" component={CreateDessert} />
         {/*<Route exact path="/home/budget" component={Budget} />
         <Route exact path="/home/contact" component={Contact} />
 
