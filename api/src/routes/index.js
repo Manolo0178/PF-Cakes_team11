b27b763bc7b0618a15ac8b0af7d6ed916d6b9505
juan huertas <juan-huertas1234@outlook.es>
2023-05-12 17:12:45 -0500

    ruta comentarios

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,16 +3,18 @@ const { Router } = require('express');
 // Ejemplo: const authRouter = require('./auth.js');
 let productRouter = require("./productDessert/productRouter.js")
 let dessertRouter = require("./productDessert/dessertRouter.js")
-/******** */
-let cartRouter = require("./carritoProduct/cartRouter.js")
+let reviewRouter = require('./reviewRouter/reviewRouter.js')
+/*********/
+// let cartRouter = require("./carritoProduct/cartRouter.js")
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use("/products", productRouter);
+router.use("/products", productRouter); // comentario ejemplo
 router.use("/desserts", dessertRouter);
+router.use('/review', reviewRouter)
 
-router.use("/carts", cartRouter)
+// router.use("/carts", cartRouter)
 
 
 module.exports = router;
