d974c39c8cf05f9c2c4ce6cd8101c4663d2a391d
horacio cano <hhoraciocano@gmail.com>
2023-05-13 17:39:36 -0300

    Ruta put y paginado desde el back terminado

--- api/src/routes/productDessert/productRouter.js
+++ api/src/routes/productDessert/productRouter.js
@@ -36,15 +36,21 @@ productRouter.get("/:idProduct", async (req, res) => {
 })
 
 
-
 productRouter.get("/", async (req, res) => {
   try {
-    const { name, size = 0, page = 10 } = req.query;
+    const { name, size = 0, page = 10, sort, sortBy } = req.query;
 
     let option = {
       limit: Number(size),
       offset: Number(page) * Number(size),
-      include: Dessert
+      include: Dessert,
+      order: []
+    };
+
+    if (sortBy && sortBy === 'price') {
+      option.order.push(['price', sort === 'desc' ? 'DESC' : 'ASC']);
+    } else {
+      option.order.push(['name', sort === 'desc' ? 'DESC' : 'ASC']);
     }
 
     if (name) {
@@ -57,45 +63,28 @@ productRouter.get("/", async (req, res) => {
           }
         },
         include: Dessert,
+        order: []
+      };
+
+      if (sortBy && sortBy === 'price') {
+        optionName.order.push(['price', sort === 'desc' ? 'DESC' : 'ASC']);
+      } else {
+        optionName.order.push(['name', sort === 'desc' ? 'DESC' : 'ASC']);
       }
-      const {count, rows} = await Product.findAndCountAll(optionName)
-      const product = rows.length
-      product ? 
-      res.json({
-        status: "succes",
-        total: count,
-        product: rows,
-        length: rows.length // esta linea sirve para ver cuantos productos traen por paginado
-      })
-      : res.status(404).json({ message: `Product name not found ${name}` })
-      // const productBDd = await Product.findAll({
-      //   where: {
-      //     name: {
-      //       [Op.iLike]: `%${name}%`,
-      //     }
-      //   },
-      //   include: Dessert,
-      // })
-      // if (!productBDd.length > 0) {
-      //   res.status(404).json({ message: `Product name not found ${name}` })
-      // } else {
-      //   res.status(200).json(productBDd)
-      // }
+
+      const { count, rows } = await Product.findAndCountAll(optionName);
+      const product = rows.length;
+
+      product ? res.json({ status: "success", total: count, product: rows, length: rows.length }) : res.status(404).json({ message: `Product name not found ${name}` });
     } else {
-      // const productBdd = await Product.findAll({ include: Dessert }) // Esta linea trae todos los productos sin paginar.
-      //   res.status(200).json(productBdd);
-      const {count, rows} = await Product.findAndCountAll(option)
-      res.json({
-        status: "succes",
-        total: count,
-        product: rows,
-        // length: rows.length // esta linea sirve para ver cuantos productos traen por paginado
-      })
-      }
-    } catch (error) {
-      res.status(500).json({ message: error.message })
+      const { count, rows } = await Product.findAndCountAll(option);
+      res.json({ status: "success", total: count, product: rows, length: rows.length });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-})
+});
+
 //se agrego un controlador que hace un pedido a una api creada por url
         
 productRouter.post("/", async (req, res) => {
@@ -133,6 +122,35 @@ productRouter.post("/", async (req, res) => {
   }
 
 })
+
+
+productRouter.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, price, description, summary, image } = req.body;
+
+  try {
+    const product = await Product.findByPk(id);
+
+    if (!product) {
+      return res.status(404).json({ message: `Product with id ${id} not found` });
+    }
+
+    product.name = name || product.name;
+    product.price = price || product.price;
+    product.description = description || product.description;
+    product.summary = summary || product.summary
+    product.image = image || product.image
+
+    await product.save();
+
+    res.json({ status: `the ${product.name} product was successfully modified`, product });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Resto del código de la ruta GET paginada...
+
 module.exports = productRouter;
       
         
