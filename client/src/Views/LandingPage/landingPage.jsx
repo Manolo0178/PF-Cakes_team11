c30c805867b819381444b6a2a6fd321d67ff86f1
Josema996 <101608821+Josema996@users.noreply.github.com>
2023-05-16 14:39:23 -0300

    landingPage

--- client/src/Views/LandingPage/landingPage.jsx
+++ client/src/Views/LandingPage/landingPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./landingPage.module.css";
+import LoginForm from "../../components/LoginForm/LoginForm";
 
 function Landing() {
   return (
@@ -16,26 +17,13 @@ function Landing() {
             />
             <h1 className="text-uppercase mb-4">Bienvenidos a nuestra pastelería</h1>
             
+            <Link to="/home">
+              <button className={styles.btn}>Ingresar</button>
+            </Link>
           </div>
           <div className={`col-md-6 ${styles.rightSide}`}>
-            <div className={`${styles.loginForm} ${styles.centeredLoginForm}`}>
-              <h2>Log in</h2>
-              <form>
-                <div className="form-group">
-                  <label htmlFor="username">User</label>
-                  <input type="text" className={`form-control ${styles.widerInput}`} id="username" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="password">Password</label>
-                  <input type="password" className={`form-control ${styles.widerInput}`} id="password" />
-                </div>
-                <br />
-                <div className={styles.buttonContainer}>
-                  <button type="submit" className={styles.btn}>Login</button>
-                  <p className="lead mb-1">or</p>
-                  <Link to="/home" ><button className={styles.btn}>login without user</button></Link>
-                </div>
-              </form>
+            <div className={styles.loginForm}>
+              <LoginForm/>
             </div>
           </div>
         </div>
