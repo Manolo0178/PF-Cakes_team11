4e8bbb1a245d59ac0cdc258f0d39aa3fd0fdcbc1
AylenRondan <rrondancarrizo@gmail.com>
2023-05-18 17:46:46 -0300

    modificacion de carrousel y about

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import carousel1 from "../../assets/images/carrousel1.jpg"
 import carousel2 from "../../assets/images/carrousel2.jpg"
 import carousel3 from "../../assets/images/carrousel3.jpg"
+import carousel4 from "../../assets/images/carrousel4.jpg"
 
 import styles from "./Carousel.module.css"
 
@@ -23,7 +24,7 @@ function ControlledCarousel() {
         <img className="d-block w-100" src={carousel2} alt="Second slide" style={{height: "500px", objectFit:"cover"}} /> 
       </Carousel.Item>
       <Carousel.Item>
-        <img className="d-block w-100" src={carousel3} alt="Third slide" style={{height: "500px", objectFit:"cover"}} />
+        <img className="d-block w-100" src={carousel4} alt="Third slide" style={{height: "500px", objectFit:"cover"}} />
       </Carousel.Item>
     </Carousel>
   );
