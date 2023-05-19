a01610e9950621463a29bf5b3f1e0069447f0bfb
horacio cano <hhoraciocano@gmail.com>
2023-05-18 21:16:45 -0300

    rutas de direcciones y usuarios terminada

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -5,14 +5,18 @@ let productRouter = require("./productDessert/productRouter.js")
 let dessertRouter = require("./productDessert/dessertRouter.js")
 let reviewRouter = require('./reviewRouter/reviewRouter.js')
 let cartRouter = require("./carritoProduct/cartRouter.js")
+let userRouter = require("./userRouter/userRouter.js")
+let addressRouter = require("./addressesRouter/addressRouter.js")
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.use("/products", productRouter); // comentario ejemplo
 router.use("/desserts", dessertRouter);
-router.use('/review', reviewRouter)
-router.use("/carts", cartRouter)
+router.use('/review', reviewRouter);
+router.use("/carts", cartRouter);
+router.use("/user", userRouter);
+router.use("/Address",addressRouter);
 
 
 module.exports = router;
