3230307adf486a2ac24d59ccffc76a3fa9366f5b
Manuel Gomez <manuel0178@hotmail.com>
2023-05-20 12:29:04 -0500

    Update brach Manuel

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -6,12 +6,7 @@ let productRouter = require("./productDessert/productRouter.js")
 let dessertRouter = require("./productDessert/dessertRouter.js")
 let reviewRouter = require('./reviewRouter/reviewRouter.js')
 let cartRouter = require("./carritoProduct/cartRouter.js")
-<<<<<<< HEAD
 let routerPago = require('./routerPagos/routerPago.js')
-=======
-let userRouter = require("./userRouter/userRouter.js")
-let addressRouter = require("./addressesRouter/addressRouter.js")
->>>>>>> ebab74f34d182125449ffe6cec53a886a24ca03c
 const router = Router();
 
 // Configurar los routers
@@ -27,7 +22,6 @@ router.use('/review', reviewRouter);
 router.use("/carts", cartRouter);
 router.use("/user", userRouter);
 router.use("/Address",addressRouter);
->>>>>>> ebab74f34d182125449ffe6cec53a886a24ca03c
 
 
 module.exports = router;
