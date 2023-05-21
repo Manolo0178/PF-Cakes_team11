269664d16ac7a09384262211c040c6d3ac0059c6
juan huertas <juan-huertas1234@outlook.es>
2023-05-20 20:12:57 -0500

    se actualizaron las rutas comentarios y se agrego ruta para eliminar y actualizar comentario

--- api/src/routes/reviewRouter/reviewRouter.js
+++ api/src/routes/reviewRouter/reviewRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const reviewRouter = express.Router();
-const { Review } = require('../../db.js')
+const { Review, User } = require('../../db.js')
 
-reviewRouter.get('/', async (req, res)=>{
+reviewRouter.get('/:productId', async (req, res)=>{
     try {
-        const allReview = await Review.findAll()
+        const { productId } = req.params;
+        const allReview = await Review.findAll({where:{productId}, include: { model: User, attributes: ['name'] } });
+
         if(!allReview.length){
             res.status(404).json({message:'there are no comments'})
         }else{
@@ -15,10 +17,49 @@ reviewRouter.get('/', async (req, res)=>{
     }
 })
 
-
-reviewRouter.post('/', async (req, res) => {
+reviewRouter.put('/:idUser/:reviewId', async (req, res) => {
     try {
-        const { comment, qualification, productId, userId } = req.body
+        const { reviewId, idUser } = req.params;
+        const { comment, qualification } = req.body;
+        
+        const review = await Review.findByPk(reviewId);
+        
+        if(review.userId === idUser){
+            await Review.update(
+                {comment, qualification},
+                {where:{id: reviewId}}
+            )
+            res.json({message: 'se actualizo el comentario'})
+        }else{
+            res.status(400).json({message: 'no autorizado'})
+        }
+    } catch (error) {
+        res.status(500).json({message:error.message})        
+    }
+})
+
+reviewRouter.delete('/:idUser/:idReview', async (req, res) => {
+    try {
+        const { idUser, idReview } = req.params;
+
+        const review = await Review.findByPk(idReview);
+    
+    console.log(review.userId, idUser);
+        if(review.userId == idUser) {
+            await review.destroy();
+            res.json({ message: 'Comentario eliminado' });
+        } else {
+            res.status(401).json({ message: 'No autorizado' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+reviewRouter.post('/:userId/:productId', async (req, res) => {
+    try {
+        const { userId, productId } = req.params;
+        const { comment, qualification } = req.body;
         const newReview = await Review.create({
             comment,
             qualification,
