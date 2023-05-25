1eafca68c9201a1761649e02844afbec418c8421
Josema996 <101608821+Josema996@users.noreply.github.com>
2023-05-25 18:58:56 -0300

    carrito

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,42 +1,54 @@
-import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import SearchBar from "../SearchBar/SearchBar";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import { MdOutlineLocalGroceryStore } from "react-icons/md";
-import Swal from "sweetalert2";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import SearchBar from '../SearchBar/SearchBar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { MdOutlineLocalGroceryStore } from 'react-icons/md';
+import Swal from 'sweetalert2';
 import { useSelector } from 'react-redux';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
-import styles from "./Navbar.module.css";
+
+import styles from './Navbar.module.css';
 
 import Cart from '../Cart/Cart';
 
 function NavBar() {
   const navigate = useNavigate();
-  const storedToken = localStorage.getItem("token");
+  const storedToken = localStorage.getItem('token');
   const [cartVisible, setCartVisible] = useState(false);
   const cartItems = useSelector((state) => state.cartItems);
   const cartItemCount = cartItems.length;
+  const cartTotal = calculateTotal();
 
   const toggleCart = () => {
-    setCartVisible(!cartVisible);
+    setCartVisible((prevVisible) => !prevVisible);
   };
 
   const logoutButton = () => {
     Swal.fire({
-      title: "¿Estás seguro de querer salir?",
-      icon: "question",
-      confirmButtonText: "Ok",
+      title: '¿Estás seguro de querer salir?',
+      icon: 'question',
+      confirmButtonText: 'Ok',
       showCancelButton: true,
     }).then((result) => {
       if (result.isConfirmed) {
-        localStorage.removeItem("token");
-        navigate("/home");
+        localStorage.removeItem('token');
+        navigate('/home');
       }
     });
   };
+
+  function calculateTotal() {
+    return cartItems.reduce((accumulator, item) => {
+      if (typeof item.price === 'number') {
+        return accumulator + item.price * item.quantity;
+      }
+      return accumulator;
+    }, 0);
+  }
 
   return (
     <Navbar expand="lg" className={styles.navBarCont}>
@@ -83,10 +95,35 @@ function NavBar() {
                 Ingresá
               </Nav.Link>
             )}
-           <Nav.Link className={styles.link} onClick={toggleCart}>
-              <MdOutlineLocalGroceryStore color="white" size="1.6rem" />
-               {cartItemCount > 0 && <span className={styles.cartItemCount}>{cartItemCount}</span>}
-          </Nav.Link>
+            <OverlayTrigger
+              placement="bottom"
+              overlay={
+                  <div className={`${styles.cartPanel} cartPanel`} >
+                    <p></p>
+                    <h5 className={styles.h5}>Resumen del Carrito</h5>
+                    <ul>
+                      {cartItems.map((item) => (
+                        <li key={item.id}>
+                          <div className={`${styles.cartItem} cartItemImage`}>
+                            <img src={item.image} alt={item.name} width="100px" />
+                            <div className={styles.itemDetails}>
+                              <h7 className={styles.h7}>{item.name}</h7>
+                              <p></p>
+                              <h6 className={styles.h6}>Cantidad: {item.quantity}</h6>
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                    <h5 className={styles.h5}>Total: ${cartTotal}</h5>
+                  </div>
+              }
+            >
+              <Nav.Link className={styles.link} onClick={toggleCart}>
+                <MdOutlineLocalGroceryStore color="white" size="1.8rem" />
+                {cartItemCount > 0 && <span className={styles.cartItemCount}>{cartItemCount}</span>}
+              </Nav.Link>
+            </OverlayTrigger>
           </Nav>
         </Navbar.Collapse>
       </Container>
