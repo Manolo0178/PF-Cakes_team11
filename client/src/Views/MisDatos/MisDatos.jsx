4f09a729fd8a30d2bc26ec81f1be4f8e0852c124
greko3450 <johannsm03@gmail.com>
2023-05-31 20:11:09 -0500

    datos modificados por inputs

--- client/src/Views/MisDatos/MisDatos.jsx
+++ client/src/Views/MisDatos/MisDatos.jsx
@@ -1,43 +1,72 @@
-import React, { useEffect } from 'react'
-import NavBar from "../../components/Navbar/Navbar";
-import Footer from "../../components/Footer/Footer";
+import React, { useEffect, useState } from 'react';
+import NavBar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import styles from "./MisDatos.module.css"
-import { useDispatch, useSelector } from "react-redux";
-import MiPerfilNav from "../miPerfil/MiPerfilNav/MiPerfilNav";
-import { getUserData } from "../../redux/actions/index";
+import style from './MisDatos.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import MiPerfilNav from '../miPerfil/MiPerfilNav/MiPerfilNav';
+import { getUserData } from '../../redux/actions/index';
 
 const MisDatos = () => {
-  const id = localStorage.getItem("userId");
-  const token = localStorage.getItem("token")
-  const dispatch = useDispatch()
+  const id = localStorage.getItem('userId');
+  const token = localStorage.getItem('token');
+  const dispatch = useDispatch();
   const perfil = useSelector((state) => state.userData);
+  const [name, setName] = useState(perfil.name);
+  const [lastName, setLastName] = useState(perfil.lastName);
+  const [email, setEmail] = useState(perfil.email);
+  const [contact, setContact] = useState(perfil.contact);
+  const [isModified, setIsModified] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
-  
   useEffect(() => {
     if (!Object.keys(perfil).length) {
       dispatch(getUserData(token, id));
     }
-  },[dispatch])
+  }, [dispatch]);
 
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+    setIsModified(true);
+  };
 
-    
-  const change = async (e, val) => {
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+    setIsModified(true);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    setIsModified(true);
+  };
+
+  const handleContactChange = (e) => {
+    setContact(e.target.value);
+    setIsModified(true);
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const url = `http://localhost:3001/user/modifyUser/${id}`;
-    let changeProp = prompt(`Qué valor desea cambiarle a ${val}?`, perfil[val]);
+    const updatedPerfil = { ...perfil, name, lastName, email, contact };
+    await axios.put(url, updatedPerfil);
+    setIsModified(false);
+    setIsEditing(false);
+    window.location.reload(true);
+  };
 
-    if (changeProp !== null) {
-      const updatedPerfil = { ...perfil, [val]: changeProp };
-      await axios.put(url, updatedPerfil);
-      window.location.reload(true)
-    }
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleFormClose = () => {
+    setIsEditing(false);
   };
 
   if (Object.keys(perfil).length === 0) {
     return (
-      <div className={styles.cont}>
+      <div className={style.cont}>
         <h1>Loading...</h1>
       </div>
     );
@@ -46,37 +75,64 @@ const MisDatos = () => {
   return (
     <div>
       <NavBar />
-      <section className={styles.sectionCont}>
+      <section className={style.sectionCont}>
         <MiPerfilNav />
-        <section className={styles.section}>
-          <h1>Mis Datos</h1>
-          <div>
-            <div className={styles.datos}>
-              <div>
-                <h6>Nombre: {perfil.name}</h6>
-                <button onClick={(e) => change(e, "name")}>cambiar</button>
+        <section className={style.section}>
+          <h1 className={style.text}>Mis Datos</h1>
+          <div className={`${style.card} ${isEditing ? style.editing : ''}`}>
+            <div className={style.form}>
+              <div className={style.field}>
+                <span>Nombre:</span>
+                {isEditing ? (
+                  <input className={style.inputEdit} type="text" id="name" value={name} onChange={handleNameChange} />
+                ) : (
+                  <div className={style.valorActual}>{perfil.name}</div>
+                )}
               </div>
-              <div>
-                <h6>Apellido: {perfil.lastName}</h6>
-                <button onClick={(e) => change(e, "lastName")}>cambiar</button>
+              <div className={style.field}>
+                <span>Apellido:</span>
+                {isEditing ? (
+                  <input className={style.inputEdit} type="text" id="lastName" value={lastName} onChange={handleLastNameChange} />
+                ) : (
+                  <div className={style.valorActual}>{perfil.lastName}</div>
+                )}
               </div>
-              <div>
-                <h6>Email: {perfil.email}</h6>
-                <button onClick={(e) => change(e, "email")}>cambiar</button>
+              <div className={style.field}>
+                <span>Email:</span>
+                {isEditing ? (
+                  <input className={style.inputEdit} type="text" id="email" value={email} onChange={handleEmailChange} />
+                ) : (
+                  <div className={style.valorActual}>{perfil.email}</div>
+                )}
               </div>
-              <div>
-                <h6>Numero de contacto: {perfil.contact}</h6>
-                <button onClick={(e) => change(e, "contact")}>cambiar</button>
+              <div className={style.field}>
+                <span>Número de contacto:</span>
+                {isEditing ? (
+                  <input className={style.inputEdit} type="text" id="contact" value={contact} onChange={handleContactChange} />
+                ) : (
+                  <div className={style.valorActual}>{perfil.contact}</div>
+                )}
               </div>
-              <div>
-                <h6>
-                  Contraseña:{" "}
-                  {<input type="text" disabled placeholder="********" />}
-                </h6>
-                <Link to={"/profile/newPassword"}>
-                  <button>cambiar</button>
+              <div className={style.field}>
+                <h6>Contraseña:</h6>
+                {/* <div className={style.inputEdit} disabled>Password Input</div> */}
+                <Link to={'/profile/newPassword'}>
+                  <div className={style.boton}>
+                    <button>cambiar</button>
+                  </div>
                 </Link>
               </div>
+              {isModified && <p>¡Cambios guardados con éxito!</p>}
+              {isEditing ? (
+                <div className={style.boton}>
+                  <button onClick={handleSubmit}>Guardar</button>
+                  <button onClick={handleFormClose}>Cerrar formulario</button>
+                </div>
+              ) : (
+                <div className={style.boton}>
+                  <button onClick={handleEditClick}>Editar</button>
+                </div>
+              )}
             </div>
           </div>
         </section>
@@ -84,6 +140,6 @@ const MisDatos = () => {
       <Footer />
     </div>
   );
-}
+};
 
-export default MisDatos
+export default MisDatos;
